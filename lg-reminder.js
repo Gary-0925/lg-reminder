ef546e1f59ce1ef848ba125d661c0d97848180da
Gary0 <Gary20120925@163.com>
2026-02-22 16:09:01 +0800

    upd ver 1.1
    refactor code

--- lg-reminder.js
+++ lg-reminder.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         lg-reminder
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  洛谷私信未读消息实时提示
 // @author       Gary0
 // @match        https://www.luogu.com.cn/*
@@ -11,64 +11,65 @@
 // @run-at       document-end
 // ==/UserScript==
 
-const msg_hsty = "lg-reminder-history", uid_list = "lg-reminder-uid";
-var local_list, local_hsty = new Map;
+// id 常量
+const main_box_id = "lg_reminder", set_btn_id = "lg_reminder_set", set_box_id = "lg_reminder_box";
 
-function get_hsty_map()
-{
-	const hsty_json = localStorage.getItem(msg_hsty);
-	const hsty_ob = JSON.parse(hsty_json);
-	const hsty_arr = Object.entries(hsty_ob);
-	var hsty_map = new Map(hsty_arr);
-	return hsty_map;
-}
-function get_hsty(uid) { return get_hsty_map().get(uid.toString()); }
-function set_hsty(uid, id)
-{
-	var nowMap = get_hsty_map();
-	nowMap.set(uid.toString(), id);
-	const hsty_ob = Object.fromEntries(nowMap);
-	const hsty_json = JSON.stringify(hsty_ob);
-	localStorage.setItem(msg_hsty, hsty_json);
-}
+// 开启 debug 特殊功能
+const is_debug = localStorage.getItem("lg_reminder_debug");
 
-function get_uid_map()
+class data // 本地储存数据类
 {
-	const uid_json = localStorage.getItem(uid_list);
-	const uid_ob = JSON.parse(uid_json);
-	const uid_arr = Object.entries(uid_ob);
-	var uid_map = new Map(uid_arr);
-	return uid_map;
-}
-function get_uid(uid) { return get_uid_map().get(uid.toString()); }
-function set_uid(uid, id)
-{
-	var nowMap = get_uid_map();
-	nowMap.set(uid.toString(), id);
-	const uid_ob = Object.fromEntries(nowMap);
-	const uid_json = JSON.stringify(uid_ob);
-	localStorage.setItem(uid_list, uid_json);
-}
-function set_uid_string(uid, id)
-{
-	if (uid === null) return;
-	var nowMap = get_uid_map();
-	nowMap.set(uid, id);
-	const uid_ob = Object.fromEntries(nowMap);
-	const uid_json = JSON.stringify(uid_ob);
-	localStorage.setItem(uid_list, uid_json);
+	constructor(id)
+	{
+		this.id = id;
+		if (localStorage.getItem(this.id) == null) localStorage.setItem(this.id, "{}");
+	}
+	get_map()
+	{
+		const data_json = localStorage.getItem(this.id);
+		const data_ob = JSON.parse(data_json);
+		const data_arr = Object.entries(data_ob);
+		const data_map = new Map(data_arr);
+		return data_map;
+	}
+	get_string(key) { return this.get_map().get(key); }
+	get(key) { return this.get_string(key.toString()); }
+	set_string(key, val)
+	{
+		var data_map = this.get_map();
+		data_map.set(key, val);
+		const data_ob = Object.fromEntries(data_map);
+		const data_json = JSON.stringify(data_ob);
+		localStorage.setItem(this.id, data_json);
+	}
+	set(key, val) { this.set_string(key.toString(), val); }
 }
 
-function escape_HTML(str)
+// 读取本都储存数据
+var msg_hsty = new data("lg-reminder-msg-hsty"), tmp_hsty = msg_hsty.get_map();
+var uid_list = new data("lg-reminder-uid-list");
+
+function esc_html(str) // 防 xxs
 {
 	return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
 }
 
-const send_chat_js =
-`
-async function send_chat(uid)
+// 元素快捷操作
+function get_el(id) { return document.getElementById(id); }
+function new_el(id) { return document.createElement(id); }
+function insert_el(base, label, content, id, classList)
 {
-	var content = prompt("请输入要回复的消息", "qqnllwb");
+	const el = new_el(label);
+	el.innerHTML = content, el.id = id, el.classList = classList;
+	return base.appendChild(el);
+}
+function insert_css(content) { insert_el(get_el(main_box_id), "style", content, "", ""); }
+function insert_js(content) { insert_el(get_el(main_box_id), "script", content, "", ""); }
+
+
+async function send_prompt_chat(uid) // 发送自定义私信
+{
+	const content = prompt("请输入要回复的消息", "qqnllwb");
 	if (content == null) return;
 	await fetch("https://www.luogu.com.cn/api/chat/new",
 	{
@@ -86,73 +87,94 @@ async function send_chat(uid)
 		method: "POST",
 	});
 }
-`;
-function open_set()
+
+function load_set_box() // 加载设置面板
 {
-	const box = document.getElementById("lg_reminder_box");
-	if (box.style.display === "none")
+	const set_box = get_el(set_box_id);
+	// 判断应隐藏或显示
+	if (set_box.style.display === "none")
 	{
-		box.style.display = "";
+		// 显示
+		set_box.style.display = "";
 
-		var map = get_uid_map();
-		for (var key of map.keys())
-			if (map.get(key) === 1)
+		// 遍历特别关注用户列表
+		for (var key of uid_list.get_map().keys())
+			if (uid_list.get_string(key) === 1)
 			{
-				const uid_box = document.createElement("div");
-				uid_box.style = "position: relative; margin: 5px; border-radius: 5px; overflow: hidden; width: 90%; padding: 10px; background-color: #45bee68a; border: 0; pointer-events: auto;";
-				uid_box.innerHTML = `<a href="https://www.luogu.com.cn/user/${key}">${key}</a>`;
+				// 特别关注用户条目
+				const uid_box = insert_el(set_box, "div", `<a href="https://www.luogu.com.cn/user/${key}">${key}</a>`, "", "lg_reminder_box");
+				uid_box.style = "position: relative; width: 90%; background-color: #45bee68a;";
 
-				const del_btn = document.createElement("button");
-				del_btn.id = "del_btn_" + key;
-				del_btn.style = "position: absolute; right: 0; top: 0; text-align: center; border-radius: 5px; overflow: hidden; height: 100%; padding: 10px; background-color: #e82214c5; border: 0; pointer-events: auto;";
-				del_btn.innerHTML = "取消特别关注";
-				del_btn.addEventListener('click', function(key){ set_uid_string(key, 0), open_set(), open_set(); }.bind(null, key));
-				uid_box.appendChild(del_btn);
+				if (is_debug)
+				{
+					// 删除最新消息记录（debug用）
+					const clr_btn = insert_el(uid_box, "button", "删除最新消息记录(debug)", "", "lg_reminder_btn");
+					clr_btn.style = "position: absolute; right: 150px; top: 0; height: 100%; background-color: #14a1e8c5; margin: 0;";
+					clr_btn.addEventListener("click", function(uid){ tmp_hsty.set(uid, 0), msg_hsty.set_string(uid, 0), load_set_box(), load_set_box(); }.bind(null, key));
+				}
 
-				box.appendChild(uid_box);
+				// 取消特别关注
+				const del_btn = insert_el(uid_box, "button", "取消特别关注", "", "lg_reminder_btn");
+				del_btn.style = "position: absolute; right: 0; top: 0; height: 100%; background-color: #e82214c5; margin: 0;";
+				del_btn.addEventListener("click", function(uid){ uid_list.set_string(uid, 0), load_set_box(), load_set_box(); }.bind(null, key));
 			}
 
-		const add_btn = document.createElement("div");
-		add_btn.style = "position: relative; margin: 5px; border-radius: 5px; overflow: hidden; width: 90%; padding: 10px; background-color: #14e84d42; border: 0; pointer-events: auto;";
-		add_btn.innerHTML = "添加特别关注用户";
-		add_btn.addEventListener('click', function(){ set_uid_string(prompt('请输入要特别关注的用户uid', 1202669), 1), open_set(), open_set(); });
-		box.appendChild(add_btn);
+		// 添加特别关注用户
+		const add_btn = insert_el(set_box, "button", "添加特别关注用户", "", "lg_reminder_btn");
+		add_btn.style = "width: 90%; background-color: #14e84d42;";
+		add_btn.addEventListener("click", function(){ uid_list.set_string(prompt('请输入要特别关注的用户uid', 1202669), 1), load_set_box(), load_set_box(); });
 
-		const cls_btn = document.createElement("button");
-		cls_btn.style = "position: fixed; right: 0; top: 0; margin: 5px; border-radius: 5px; overflow: hidden; padding: 10px; color: #e82214af; background-color: #00000000; border: 0; pointer-events: auto;";
-		cls_btn.innerHTML = "×";
-		cls_btn.addEventListener('click', function(){ open_set(); });
-		box.appendChild(cls_btn);
+		// 关闭按钮
+		const cls_btn = insert_el(set_box, "button", "×", "", "lg_reminder_btn");
+		cls_btn.style = "position: fixed; right: 0; top: 0; color: #e82214af; background-color: #00000000;";
+		cls_btn.addEventListener("click", function(){ load_set_box(); });
 	}
-	else box.style.display = "none", box.innerHTML = ``, local_list = get_uid_map();
+	else
+	{
+		// 隐藏
+		set_box.style.display = "none", set_box.innerHTML = ``;
+	}
 }
 
-function updata_msg()
+function updata_msg() // 新消息监听
 {
-	console.log(local_list.keys());
-	for (var key of local_list.keys())
-		if (local_list.get(key) === 1)
+	console.log("lg_reminder 新消息监听");
+
+	for (var key of uid_list.get_map().keys())
+		if (uid_list.get(key) === 1)
 		{
-			console.log(key);
+			// 获取消息列表
 			fetch("https://www.luogu.com.cn/api/chat/record?user=" + key)
 			.then((response) => response.json())
 			.then((data) =>
 			{
-				const msg_box = document.getElementById("lg_reminder");
+				const main_box = get_el(main_box_id);
+
+				// 获取消息列表中最新消息
 				const last_message = data.messages.result[data.messages.result.length - 1];
-				if (local_list.get(last_message.sender.uid.toString()) === 1 && (get_hsty(last_message.sender.uid) != last_message.id) && (local_hsty.get(last_message.sender.uid.toString()) != last_message.id))
+
+				// 判断是否为新消息
+				if ((uid_list.get(last_message.sender.uid) === 1) && (msg_hsty.get(last_message.sender.uid) != last_message.id) && (tmp_hsty.get(last_message.sender.uid.toString()) != last_message.id))
 				{
-					if (window.location.pathname.includes("chat") && !window.location.pathname.includes("api")) set_hsty(last_message.sender.uid, last_message.id);
+					if (window.location.pathname.includes("chat") && !window.location.pathname.includes("api"))
+					{
+						// 将消息设置为非新消息
+						msg_hsty.set(last_message.sender.uid, last_message.id);
+					}
 					else
 					{
-						local_hsty.set(last_message.sender.uid.toString(), last_message.id);
-						const new_msg_box = document.createElement("div");
-						new_msg_box.style = "border-radius: 5px; margin: 5px; overflow: hidden; width: 90%; padding: 10px; background-color: #f0f5ffdd; pointer-events: auto;";
+						// 防止消息弹出多次
+						tmp_hsty.set(last_message.sender.uid.toString(), last_message.id);
+
+						// 长消息处理
 						if (last_message.content.length >= 10)
-							last_message.content = escape_HTML(last_message.content.substr(0, 8)) + `<a href='javascript:alert("${escape_HTML(last_message.content)}")'>...</a>`;
-						else last_message.content = escape_HTML(last_message.content);
-						new_msg_box.innerHTML = `<a href="javascript:send_chat(${last_message.sender.uid})">${escape_HTML(last_message.sender.name)}</a> : ${last_message.content}`;
-						msg_box.appendChild(new_msg_box);
+							last_message.content = esc_html(last_message.content.substr(0, 8)) + `<a href='javascript:alert("${esc_html(last_message.content)}")'>...</a>`;
+						else last_message.content = esc_html(last_message.content);
+						
+						// 新消息弹窗
+						const new_msg_box = insert_el(main_box, "div", `${esc_html(last_message.sender.name)} : ${last_message.content}`, "", "lg_reminder_box");
+						new_msg_box.style = "width: 90%;";
+						new_msg_box.addEventListener("click", function(uid){ setTimeout(() => send_prompt_chat(uid), 100); }.bind(null, last_message.sender.uid));
 					}
 				}
 			})
@@ -167,37 +189,24 @@ function updata_msg()
 (function()
 {
 	'use strict';
-	if (localStorage.getItem(msg_hsty) == null) localStorage.setItem(msg_hsty, "{}");
-	if (localStorage.getItem(uid_list) == null) localStorage.setItem(uid_list, "{}");
-	local_hsty = get_hsty_map();
-	local_list = get_uid_map();
 	setTimeout(() =>
 	{
-		// 插入主面板 lg_reminder
-		const lg_reminder_main = document.createElement("div");
-		lg_reminder_main.id = "lg_reminder";
-		lg_reminder_main.style = "position: fixed; top: 100px; right: 0; height: 100%; width: 200px; z-index: 1000; pointer-events: none;";
-		document.body.appendChild(lg_reminder_main);
+		// 主容器
+		const main_box = insert_el(document.body, "div", "", main_box_id, "lg_reminder");
+		main_box.style = "position: fixed; top: 100px; right: 0; height: 100%; width: 200px; z-index: 1000; pointer-events: none;";
 
-		// 插入发送私信脚本
-		const send_chat_script = document.createElement("script");
-		send_chat_script.innerHTML = send_chat_js;
-		lg_reminder_main.appendChild(send_chat_script);
+		// css
+		insert_css(`.lg_reminder_box{ overflow: hidden; margin: 5px; padding: 10px; border-radius: 10px; background-color: #f0f5ffdd; pointer-events: auto; }`);
+		insert_css(`.lg_reminder_btn{ overflow: hidden; margin: 5px; padding: 10px; border-radius: 10px; background-color: #f0f5ffdd; pointer-events: auto; text-align: center; border: 0; }`);
 
-		// 插入设置面板 lg_reminder_box
-		const lg_reminder_box = document.createElement("div");
-		lg_reminder_box.id = "lg_reminder_box";
-		lg_reminder_box.style = "position: fixed; display: none; border-radius: 10px; padding: 10px; top: 0; left: 0; height: 100%; width: 100%; background-color: #f0f5ffdd; pointer-events: auto;";
-		lg_reminder_box.innerHTML = "";
-		lg_reminder_main.appendChild(lg_reminder_box);
+		// 设置面板
+		const set_box = insert_el(main_box, "div", "", set_box_id, "lg_reminder_box");
+		set_box.style = "position: fixed; display: none; top: 0; left: 0; height: 100%; width: 100%; padding-top: 50px; margin: 0;";
 
-		// 插入设置按钮 lg_reminder_set
-		const lg_reminder_set = document.createElement("button");
-		lg_reminder_set.id = "lg_reminder_set";
-		lg_reminder_set.style = "text-align: center; margin: 5px; border-radius: 5px; overflow: hidden; width: 90%; padding: 10px; background-color: #f0f5ffdd; border: 0; pointer-events: auto;";
-		lg_reminder_set.innerHTML = "设置";
-		lg_reminder_set.addEventListener('click', function(){ open_set(); });
-		lg_reminder_main.appendChild(lg_reminder_set);
+		// 设置按钮
+		const set_btn = insert_el(main_box, "button", "设置", set_btn_id, "lg_reminder_btn");
+		set_btn.style = "width: 90%;";
+		set_btn.addEventListener("click", function(){ load_set_box(); });
 
 		// 启动监听
 		updata_msg();
